60fa2a822cf0eda9c6c129107d995b6c1aef13a5
Art Weinstein <artur.weinstein@gmail.com>
2021-09-07 16:27:20 -0700

    add columns and trending option for pulling up gifs
    Co-authored-by: Chynna Lew <chynnalew@yahoo.com>
    
    Co-authored-by: Erica Marroquin <ericamarroquin03@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,3 +2,56 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+
+$(document).ready(function() {
+  $('#findGif').click(function() {
+    const searchWord = $('#gif').val();
+    $('#gif').val("");
+
+    let request = new XMLHttpRequest();
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${searchWord}&limit=5&offset=0&rating=g&lang=en`;
+
+    request.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
+    function getElements(response) {
+      let imageUrl = response.data[0].images.original.url;
+      $('.img1').html('<img src="' + imageUrl + '">');
+      $('.img2').html(`<img src=" ${response.data[1].images.original.url}">`);
+      $('.img3').html(`<img src=" ${response.data[2].images.original.url}">`);
+      $('.img4').html(`<img src=" ${response.data[3].images.original.url}">`);
+    }
+  });
+  $('#trending').click(function() {
+    
+    let request = new XMLHttpRequest();
+    const url = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=4&offset=0&rating=r&lang=en`;
+
+    request.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
+    function getElements(response) {
+      $('.trend1').html(`<img src=" ${response.data[0].images.original.url}">`);
+      $('.trend2').html(`<img src=" ${response.data[1].images.original.url}">`);
+      $('.trend3').html(`<img src=" ${response.data[2].images.original.url}">`);
+      $('.trend4').html(`<img src=" ${response.data[3].images.original.url}">`);
+    }
+  })
+})
+
+// $('.img1').html(`<img src="${response.data[0].images.original.url}"`)
